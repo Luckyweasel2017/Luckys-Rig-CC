95983b8fa8fd28b7c5dddadc11853b421b89a93d
Luckyweasel2017 <luckyshdtube@gmail.com>
2018-03-25 06:37:43 +0200

    Create start-remote.sh

---
+++ start-remote.sh
@@ -0,0 +1,13 @@
+## Lucky's XMR-CC 2018 ##
+
+sudo ./xmrig /
+  --api-port 80 /
+  --av 2 /
+  --safe /
+  --cpu-priority 4 /
+  -o pool.electroneum.hashvault.pro:3333 /
+  -u etnk2jC5sr51htNg5dQmzhJZrFVn7dxBjM2Sc6Y7kW3cTHbHUv18TFB8KiBR6uofRsbYSwFBdbT49Gdqxbay8p1R2WqfF2K8a8 /
+  -p CPU-2:luckyshdtube@gmail.com /
+  -k
+  
+  ## ENDE ##
